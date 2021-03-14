f9c8383d51dd7aa4cfc4d0828148f874f92a5945
GuillaumeDJD <g.djaid@lecolededesign.com>
2021-03-14 19:59:22 +0100

    Tentative de grossir et grandire une élipse

--- sketch.js
+++ sketch.js
@@ -1,13 +1,39 @@
+let width = 20
+let height = 20
+const minWidth = 20;
+ const maxWidth = 40;
+ const minHeight = 20
+ const maxHeight = 40
+let gettingLarger = true;
+let gettingHigher = true;
+
 function setup() {
   createCanvas(1265, 670);
-  background(240);
 }
 
 function draw() {
+  background(240)
   if (mouseIsPressed) {
+    ellipse(width/2,height/2);
     fill(35,30,98);
-  } else {
-    fill(240,45,75);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+    
+if(gettingLarger){
+  width = width + 2;    
+}else{
+  width = width - 2;
+if(gettingLarger){
+  height = height + 2;    
+}else{
+  height = height - 2;
 }
+
+if(width < minWidth){
+  gettingLarger = true;   
+}else if(width > maxWidth){
+  gettingLarger = false;
+}
+if(height < minHeight){
+  gettingLarger = true;   
+}else if(height > maxHeight){
+  gettingLarger = false;
+} 
